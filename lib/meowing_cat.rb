53763a31dd15a11a23a7fbb29451e20fb5ec14d0
stanford kangethe <stanfordkangethe16@gmail.com>
2022-08-23 05:30:42 +0300

    Macros introduction

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,15 @@
 ## code your solution here. 
+class Cat
+  attr_accessor :name
+
+  def meow
+    puts "meow!"
+  end
+
+end
+
+lucyka = Cat.new
+lucyka.name = "Lucyka"
+
+puts lucyka.name
+lucyka.meow
